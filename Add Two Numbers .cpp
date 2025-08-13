10593126472cef24d0ddb2b91386aa114910c525
Davin4919 <116618005+Davin4919@users.noreply.github.com>
2025-08-13 12:21:26 -0700

    Add files via upload

---
+++ Add Two Numbers .cpp
@@ -0,0 +1,18 @@
+// Created by Daniel A 8/13/2025.
+//Add two numbers.cpp
+
+#include <iostream>
+using namespace std;
+int main() {
+    int sumOfNums = 0;
+    int num1 = 0;
+    int num2 = 0;
+    cout << " Enter a value for num1";
+    cin >> num1;
+    cout << " Enter a whole number for num2";
+    cin >> num2;
+    sumOfNums = num1 + num2;
+    cout << " The sum of " << num1 << " and " << ""
+    << num2 << " is " << sumOfNums << endl;
+    return 0;
+}

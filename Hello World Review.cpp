5d2232cbd36e6995d56306aa887750e0e251f238
Davin4919 <116618005+Davin4919@users.noreply.github.com>
2025-08-13 11:46:38 -0700

    Add files via upload

---
+++ Hello World Review.cpp
@@ -0,0 +1,17 @@
+//Hello World Review
+// Daniel A 8.13.25
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!!" << endl;
+
+    int num1 = 22;
+    int num2 = 23;
+    cout << "The value of num1 is" << num1 << endl;
+
+    cout << "The value of num2 is" << num2 << endl;
+
+    return 0;
+}

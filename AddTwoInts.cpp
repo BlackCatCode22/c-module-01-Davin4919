133776224f6d971e038df2f5c49168d19d28fc9b
Davin4919 <116618005+Davin4919@users.noreply.github.com>
2025-08-18 12:12:00 -0700

    Add files via upload

---
+++ AddTwoInts.cpp
@@ -0,0 +1,21 @@
+// Add two ints
+// Created by Daniel A on 8/18/2025.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int num1;
+    int num2;
+    int sumOfNums;
+    num1 = num2 = sumOfNums;
+    cin >>  num1;
+    cin >> num2;
+    cout << sumOfNums << endl;
+
+
+
+
+
+
+ return 0;
+}
